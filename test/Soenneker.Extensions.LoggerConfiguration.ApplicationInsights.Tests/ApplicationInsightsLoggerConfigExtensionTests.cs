1e01e14ffdd61cb6013fa194c80bd8cb70f96228
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:21 +0000

    Tests

---
+++ test/Soenneker.Extensions.LoggerConfiguration.ApplicationInsights.Tests/ApplicationInsightsLoggerConfigExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.LoggerConfiguration.ApplicationInsights.Tests;
+
+public class ApplicationInsightsLoggerConfigExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
